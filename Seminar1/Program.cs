a836bdf5ca667104bb67b032d01f2d83849eb373
David Freeman <davidfreeman2022@gmail.com>
2023-03-12 12:23:27 +0300

    First seminar ended

---
+++ Seminar1/Program.cs
@@ -0,0 +1,78 @@
+﻿// /* int num = -12;
+//  double bigNum = 12.13;
+//  bool isEven = true;
+//  string word = "flower";
+//  char symbol = '.';
+
+// */
+
+// // int num1 = -12;
+// // int num2 = 6;
+
+// // Console.WriteLine("My number are " +num1 +" and "+ num2 + "and it is good!" );
+// // Console.WriteLine($"My number are {num1} and {num2}  and it is good!" );
+
+//  Console.Write("Введите перое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int quad2 = num2 * num2;
+
+// if(num1 == quad2)
+
+ 
+// Console.Write("YES!");
+ 
+// else
+
+
+// Console.Write("NO!");
+  
+
+//  ЗАДАЧА № 1
+
+// int CutNumber(int num)
+// {
+//      int hundreds = num / 100;
+//      int units = num % 10;
+//      int result = hundreds * 10 + units;
+
+//      return result;
+// }
+
+// int randNumber = new Random().Next(100,1000);
+
+// int newNumber = CutNumber(randNumber);
+
+// Console.WriteLine($"New version of {randNumber} is {newNumber}");
+
+// ЗАДАЧА №2   
+
+//bool IsMultiplyed(int num, int div1, int div2)
+//{
+// 
+//    if(num % div1 == 0)
+//
+//}
+// Задача №2 Напишите программу, которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Input a positive number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int current = -num;
+while(current <= num)
+{
+    Console.Write(current +" ");
+    current ++;
+}
+
+// 31542 / 10 =3154
+// 31542 / 100 = 315
+// 31542 / 1000 = 31
+// 31542 / 10000 = 3
+
+// 31542 % 10 =2
+// 31542 % 100 = 42
+// 31542 % 1000 = 542
+// 31542 % 1000 =1542
